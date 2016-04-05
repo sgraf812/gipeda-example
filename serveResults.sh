10008fc4aefbb94584cdc59abd8cb8be026a8676
Sebastian Graf <sgraf1337@gmail.com>
2016-04-05 09:45:15 +0200

    Simplified the setup

--- serveResults.sh
+++ serveResults.sh
@@ -14,16 +14,14 @@ rm -rf site/
 cp -r $p/site ./site
 sh $p/install-jslibs.sh
 
-# 3. req: logs folder with the raw data to visualize
-rm -rf logs
-mkdir logs/
-stack cloben.hs ./repository 77813a976b5a7d73ffdef224e6175254e0cca5b3 > logs/77813a976b5a7d73ffdef224e6175254e0cca5b3.log
+# 3. req: site/out/results folder with the raw data to visualize
+mkdir -p site/out/results
+stack cloben.hs ./repository 77813a976b5a7d73ffdef224e6175254e0cca5b3 > site/out/results/77813a976b5a7d73ffdef224e6175254e0cca5b3.csv
 
-# 4. This will convert the raw data in *.log files to the expected CSV format by using the local log2csv script. That's just calling cat for us, since the logs contain already the CSVs.
-# Note that the gipeda executable has to be present (at least a link) in ./.
-chmod +x ./log2csv
+# 4. gipeda supports 3 different `LogSource`s (grep for that in gipeda.hs). 2 of them require a ./logs/ directory (either a git repository or not) containing <SHA>.log files for each benchmarked commit SHA. gipeda will call ./log2csv on each of them which will produce appropriate CSV files with the data. The 3rd `LogSource` mode used here is to not create a ./log/ directory at all and instead directly write out the CSV output of cloben into ./site/out/results. That way, we don't have to provide a ./log2csv script.
+# Due to unwieldy circumstances in the implementation of gipeda, we have to provide a link to it in ./:
 ln $p/gipeda ./gipeda
-./gipeda -j
+$p/gipeda -j
 
 # 5. Start a server at ./site. Just accessing site/index.html with file:// doesn't work because of cross site requests.
 cd site
